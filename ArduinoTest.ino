dd3f280164a4c85ad38ebbc36a9e603f7ea87204
kar-ashishman <kar.ashishman@gmail.com>
2026-09-13 12:06:59 +0530

    Update ArduinoTest.ino

--- ArduinoTest.ino
+++ ArduinoTest.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 #define BOOTLOADERVER 0x12
-#define SECRET_CODE   0xABCD1234
+#define SECRET_CODE   0xABCD1234U
 
 const uint32_t AUTH_CMD = 0x03;
 const uint32_t SEED     = 0x12345678;
@@ -81,78 +81,40 @@ void processTestTarget() {
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
     delay(480);
   }
+  Serial.write(0);
 }
 
 void processAuthorization() {
-  unsigned long seed, key, rcvdKey;
+  char match = 1;
+  unsigned long seed, key; 
+  unsigned char rcvdKey[4];
   // accept the bootloader version request by a 0 response
+  delay(100);
   Serial.write(0);
+  delay(100);
   // create seed as current time in milliseconds.
   seed = millis();
-  key = (seed ^ SECRET_CODE) & 0xFFFFFFFF;
-  Serial.write(key & 0xFF);
-  Serial.write((key & 0xFF00) >> 8);
-  Serial.write((key & 0xFF0000) >> 16);
-  Serial.write((key & 0xFF000000) >> 24);
+  key = (unsigned long)(seed ^ SECRET_CODE);
+  Serial.write(seed & 0xFF);
+  Serial.write((seed & 0xFF00) >> 8);
+  Serial.write((seed & 0xFF0000) >> 16);
+  Serial.write((seed & 0xFF000000) >> 24);
 
+  // wait for a serial message
   while(!Serial.available());
-  rcvdKey = Serial.read() << 24;
-  rcvdKey = rcvdKey | (Serial.read() << 16);
-  rcvdKey = rcvdKey | (Serial.read() << 8);
-  rcvdKey = rcvdKey | Serial.read();
+    rcvdKey[0] = Serial.read();
+  while(!Serial.available());
+    rcvdKey[1] = Serial.read();
+  while(!Serial.available());
+    rcvdKey[2] = Serial.read();
+  while(!Serial.available());
+    rcvdKey[3] = Serial.read();
 
   // match key
-  if(key == rcvdKey) Serial.write(0);
+  match = rcvdKey[0] == (key & 0xFF);
+  match &= rcvdKey[1] == ((key & 0xFF00) >> 8);
+  match &= rcvdKey[2] == ((key & 0xFF0000) >> 16);
+  match &= rcvdKey[3] == ((key & 0xFF000000) >> 24);
+  if(match) Serial.write(0);
   else Serial.write(-1);
 }
-
-
-     /*   
-
-        if (cmd == AUTH_CMD)
-        {
-            // Authorization accepted
-            Serial.write((int8_t)0);
-
-            delay(1000);
-
-            uint32_t seed = SEED;
-
-            expectedKey = (seed ^ XOR_KEY) & 0xFFFFFFFF;
-
-            // Send seed (little-endian)
-            Serial.write((uint8_t *)&seed, sizeof(seed));
-
-            // Wait for 4-byte key
-            uint8_t keyBytes[4];
-            uint32_t timeoutStart = millis();
-
-            while (Serial.available() < 4)
-            {
-                if (millis() - timeoutStart > 5000)
-                {
-                    return;
-                }
-            }
-
-            Serial.readBytes(keyBytes, 4);
-
-            uint32_t receivedKey =
-                ((uint32_t)keyBytes[0]) |
-                ((uint32_t)keyBytes[1] << 8) |
-                ((uint32_t)keyBytes[2] << 16) |
-                ((uint32_t)keyBytes[3] << 24);
-
-            if (receivedKey == expectedKey)
-            {
-                int8_t success = 0;
-                Serial.write((uint8_t *)&success, 1);
-            }
-            else
-            {
-                int8_t fail = -1;
-                Serial.write((uint8_t *)&fail, 1);
-            }
-        }
-    }
-} */
